d866bce188add4271a5c41ada59ce1826dd54f86
artugatuga <148588412+artugatuga@users.noreply.github.com>
2023-11-12 21:13:09 +0000

    Criando folders - Tomás

---
+++ main.lua
@@ -0,0 +1,10 @@
+require "/MainCharacter/mainCharacter"
+require "/Ghosts/ghosts"
+require "/Death/death"
+require "/Valkyries/valkyries"
+require "/InGameMenu/inGameMenu"
+require "/MainMenu/mainMenu"
+
+function love.update()
+    print("hello")
+end
