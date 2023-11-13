ffd9337e27158379d878666275b71f9144b6d212
artugatuga <148588412+artugatuga@users.noreply.github.com>
2023-11-13 20:04:43 +0000

    Valquiria - Tomás

---
+++ Valkyries/valkyries.lua
@@ -0,0 +1,128 @@
+local valquiria
+require "vector"
+
+local world
+local enemy
+local enemyRange
+local ground
+local ex
+local variavel
+local lastPposition
+local time = 0
+local playerPosition
+
+function LoadValquiria()
+
+  world = love.physics.newWorld(0, 0, true)
+  ex = 50
+
+  enemy = {}
+  enemy.body = love.physics.newBody(world, ex, 100,"dynamic")
+  enemy.shape = love.physics.newRectangleShape(30, 60)
+  enemy.fixture = love.physics.newFixture(enemy.body, enemy.shape, 1)
+  enemy.maxvelocity = 200
+  enemy.isChasing = false
+  enemy.lostPlayer = false
+  enemy.patroling = true
+  enemy.playerInSight = true
+  enemy.fixture:setFriction(10)
+  enemy.body:setFixedRotation(true)
+  enemy.position = vector.new(enemy.body:getPosition())
+
+  enemyRange = {}
+  enemyRange.body = love.physics.newBody(world, enemy.body:getX(), enemy.body:getY(),"dynamic")
+  enemyRange.shape = love.physics.newCircleShape(300)
+  
+end
+
+function UpdateValquiria(dt, playerPosition)
+    
+
+  world:update(dt)
+  enemy.position = vector.new(enemy.body:getPosition())
+
+  enemyRange.body:destroy()
+  enemyRange.body = love.physics.newBody(world, enemy.body:getX(), enemy.body:getY(),"dynamic")
+
+  enemy.range = vector.magnitude(vector.sub(enemy.position, playerPosition))
+
+  if enemy.patroling == true then
+    --Check if Player in sight
+    if enemy.range < 300 then
+      enemy.playerInSight = true
+      enemy.patroling = false
+    end
+    --If not in Sight, Patrol
+    ex = enemy.body:getX()
+
+    if ex >= 1900 then
+      variavel = -1
+    end
+
+    if ex <= 100 then
+      variavel = 1
+    end
+    ex = ex + (dt * 200 * variavel)
+
+    if 95 < enemy.body:getY() and enemy.body:getY() < 105 then
+      enemy.body:setPosition(enemy.position.x, 100)
+    elseif enemy.body:getY() > 100 then
+      enemy.body:setLinearVelocity(0, -200)
+    elseif enemy.body:getY() < 100 then
+      enemy.body:setLinearVelocity(0, 200)
+    end
+
+    enemy.body:setPosition(ex, enemy.body:getY())
+
+  elseif enemy.playerInSight == true  then
+    --check again if player in sight
+    if enemy.range > 300 then
+      enemy.isChasing = false
+    else
+      enemy.isChasing = true
+    end
+
+    if enemy.isChasing == false then
+      --go to last location of player
+      local lastPos = vector.magnitude(vector.sub(enemy.position, lastPposition))
+
+      if lastPos < 1 then
+        time = time + dt
+        enemy.body:setLinearVelocity(0, 0)
+        if time > 2 then
+          enemy.patroling = true
+          enemy.playerInSight =false
+          time = 0
+          return
+        end
+        return
+      elseif lastPos > 1 then
+        local playerDiretion = vector.sub(lastPposition, vector.new(enemy.body:getPosition()))
+        playerDiretion = vector.normalize(playerDiretion)
+        local force = vector.mult(playerDiretion, 200)
+        enemy.body:setLinearVelocity(force.x, force.y)
+      end
+      
+    elseif enemy.isChasing == true then
+      --Follow player
+      time = 0
+      lastPposition = playerPosition
+      local playerDiretion = vector.sub(playerPosition, vector.new(enemy.body:getPosition()))
+      playerDiretion = vector.normalize(playerDiretion)
+      local force = vector.mult(playerDiretion, 200)
+      enemy.body:setLinearVelocity(force.x, force.y)
+    end
+  end
+end
+
+function DrawValquiria()
+  
+  love.graphics.setColor(1,1,1)
+  love.graphics.polygon("fill", enemy.body:getWorldPoints(enemy.shape:getPoints()))
+  
+  love.graphics.circle("line", enemyRange.body:getX(), enemyRange.body:getY(), enemyRange.shape:getRadius())
+end
+
+function GetValquiriaPosition()
+    return enemy.body:getPosition()
+end
--- main.lua
+++ main.lua
@@ -9,14 +9,28 @@ local world
 
 function love.load()
   world = love.physics.newWorld(0, 0, true)
+  love.window.setMode(1920, 1080)
   --Call "load" function of every script
+  CreatePlayer()
+  LoadValquiria()
 end
 
-function love.update()
-    world:update()
-    --Call update function of every script
+function love.update(dt)
+  world:update(dt)
+
+  GetWorld()
+
+  UpdatePlayer(dt)
+  UpdateValquiria(dt, GetPlayerPosition())
+  --Call update function of every script
 end
 
 function love.draw()
-    --Call draw function of every script
+  --Call draw function of every script
+  DrawPlayer()
+  DrawValquiria()
+end
+
+function GetWorld()
+  return world
 end
