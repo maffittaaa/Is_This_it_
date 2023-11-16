905c77f69648fc6a65e7212da5279da04a38eb1d
artugatuga <148588412+artugatuga@users.noreply.github.com>
2023-11-16 16:38:33 +0000

    Continuando o AI do companion - Tomás

--- Companion/companionBehavior.lua
+++ Companion/companionBehavior.lua
@@ -1,45 +1,57 @@
 require "vector"
 
-companion = {}
+local companion = {}
 local book = {}
+local wall = {}
+local wall1 = {}
+local wall2 = {}
+local wall3 = {}
+local obstacle1 = {}
 local gotBook
 local inicialPlayerPosition
 
 function LoadCompanion(world, playerPosition)
   inicialPlayerPosition = playerPosition
   gotBook = false
-  companion.body = love.physics.newBody(world, 100, 100,"dynamic")
+  companion.body = love.physics.newBody(world, 100, 540,"dynamic")
   companion.shape = love.physics.newCircleShape(30)
+  companion.fixture = love.physics.newFixture(companion.body, companion.shape, 1)
   companion.position = vector.new(companion.body:getPosition())
+  companion.fixture:setUserData("wall")
 
-  book.body = love.physics.newBody(world, 1000, 100,"dynamic")
+  book.body = love.physics.newBody(world, 1800, 540,"dynamic")
   book.shape = love.physics.newRectangleShape(30, 30)
   book.position = vector.new(book.body:getPosition())
 
   wall = {}
   wall.body = love.physics.newBody(world, 960, 15,"static")
   wall.shape = love.physics.newRectangleShape(1920, 30)
-  wall.fixture = love.physics.newFixture(wall.body, wall.shape, 1)  
+  wall.fixture = love.physics.newFixture(wall.body, wall.shape, 1)
+  wall.fixture:setUserData("wall")
   
   wall1 = {}
   wall1.body = love.physics.newBody(world, 960, 1065,"static")
   wall1.shape = love.physics.newRectangleShape(1920, 30)
   wall1.fixture = love.physics.newFixture(wall1.body, wall1.shape, 1)
+  wall1.fixture:setUserData("wall")
 
   wall2 = {}
   wall2.body = love.physics.newBody(world, 15, 540,"static")
   wall2.shape = love.physics.newRectangleShape(30, 1080)
   wall2.fixture = love.physics.newFixture(wall2.body, wall2.shape, 1)
-
-  wall2 = {}
-  wall2.body = love.physics.newBody(world, 15, 540,"static")
-  wall2.shape = love.physics.newRectangleShape(30, 1080)
-  wall2.fixture = love.physics.newFixture(wall2.body, wall2.shape, 1)
+  wall2.fixture:setUserData("wall")
 
   wall3 = {}
   wall3.body = love.physics.newBody(world, 1905, 540,"static")
   wall3.shape = love.physics.newRectangleShape(30, 1080)
   wall3.fixture = love.physics.newFixture(wall3.body, wall3.shape, 1)
+  wall3.fixture:setUserData("wall")
+
+  obstacle1 = {}
+  obstacle1.body = love.physics.newBody(world, 960, 750,"static")
+  obstacle1.shape = love.physics.newRectangleShape(30, 1080)
+  obstacle1.fixture = love.physics.newFixture(obstacle1.body, obstacle1.shape, 1)
+  obstacle1.fixture:setUserData("obstacles")
 end
 
 function UpdateCompanion(dt, playerPosition)
@@ -47,6 +59,12 @@ function UpdateCompanion(dt, playerPosition)
   companion.position = vector.new(companion.body:getPosition())
   book.position = vector.new(book.body:getPosition())
 
+  local playerDiretion = vector.sub(book.position, companion.position)
+  playerDiretion = vector.normalize(playerDiretion)
+  local force = vector.mult(playerDiretion, 200)
+
+
+  companion.body:setLinearVelocity(force.x, force.y)
   
 
   -- if gotBook == false then
@@ -89,6 +107,7 @@ function DrawCompanion()
   love.graphics.polygon("line", wall1.body:getWorldPoints(wall1.shape:getPoints()))
   love.graphics.polygon("line", wall2.body:getWorldPoints(wall2.shape:getPoints()))
   love.graphics.polygon("line", wall3.body:getWorldPoints(wall3.shape:getPoints()))
+  love.graphics.polygon("line", obstacle1.body:getWorldPoints(obstacle1.shape:getPoints()))
   if gotBook == false then
     love.graphics.polygon("line", book.body:getWorldPoints(book.shape:getPoints()))
   end 
--- main.lua
+++ main.lua
@@ -12,7 +12,8 @@ local width
 
 function love.load()
   world = love.physics.newWorld(0, 0, true)
-  
+  world:setCallBacks(BeginContact, EndContact, nil, nil)
+
   love.window.setMode(1920, 1080)
   height = love.graphics.getHeight()
   width = love.graphics.getWidth()
@@ -20,6 +21,28 @@ function love.load()
   --Call "load" function of every script
   CreatePlayer(world)
   LoadCompanion(world, PlayerPosition())
+end
+
+function BeginContact(fixtureA, fixtureB)
+  if fixtureA:getUserData() == "wall" and fixtureB:getUserData() == "Companion" then
+    print("hit wall")
+  end
+
+  if fixtureA:getUserData() == "obstacles" and fixtureB:getUserData() == "Companion" then
+    print("hit obstacles")
+  end
+
+  if fixtureA:getUserData() == "Companion" and fixtureB:getUserData() == "wall" then
+    print("hit wall")
+  end
+
+  if fixtureA:getUserData() == "Companion" and fixtureB:getUserData() == "obstacles" then
+    print("hit obstacles")
+  end
+end
+
+function EndContact(fixtureA, fixtureB)
+  
 end
 
 function love.update(dt)
