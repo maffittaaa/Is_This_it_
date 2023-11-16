efd6ac110fc3e094f7144619243867bf7145fa62
artugatuga <148588412+artugatuga@users.noreply.github.com>
2023-11-16 12:07:33 +0000

    Companion "AI" - Tomás

--- Companion/companionBehavior.lua
+++ Companion/companionBehavior.lua
@@ -8,13 +8,38 @@ local inicialPlayerPosition
 function LoadCompanion(world, playerPosition)
   inicialPlayerPosition = playerPosition
   gotBook = false
-  companion.body = love.physics.newBody(world, 1800, 100,"dynamic")
+  companion.body = love.physics.newBody(world, 100, 100,"dynamic")
   companion.shape = love.physics.newCircleShape(30)
   companion.position = vector.new(companion.body:getPosition())
 
   book.body = love.physics.newBody(world, 1000, 100,"dynamic")
   book.shape = love.physics.newRectangleShape(30, 30)
   book.position = vector.new(book.body:getPosition())
+
+  wall = {}
+  wall.body = love.physics.newBody(world, 960, 15,"static")
+  wall.shape = love.physics.newRectangleShape(1920, 30)
+  wall.fixture = love.physics.newFixture(wall.body, wall.shape, 1)  
+  
+  wall1 = {}
+  wall1.body = love.physics.newBody(world, 960, 1065,"static")
+  wall1.shape = love.physics.newRectangleShape(1920, 30)
+  wall1.fixture = love.physics.newFixture(wall1.body, wall1.shape, 1)
+
+  wall2 = {}
+  wall2.body = love.physics.newBody(world, 15, 540,"static")
+  wall2.shape = love.physics.newRectangleShape(30, 1080)
+  wall2.fixture = love.physics.newFixture(wall2.body, wall2.shape, 1)
+
+  wall2 = {}
+  wall2.body = love.physics.newBody(world, 15, 540,"static")
+  wall2.shape = love.physics.newRectangleShape(30, 1080)
+  wall2.fixture = love.physics.newFixture(wall2.body, wall2.shape, 1)
+
+  wall3 = {}
+  wall3.body = love.physics.newBody(world, 1905, 540,"static")
+  wall3.shape = love.physics.newRectangleShape(30, 1080)
+  wall3.fixture = love.physics.newFixture(wall3.body, wall3.shape, 1)
 end
 
 function UpdateCompanion(dt, playerPosition)
@@ -60,6 +85,10 @@ end
 
 function DrawCompanion()
   love.graphics.circle("line", companion.position.x, companion.position.y, companion.shape:getRadius())
+  love.graphics.polygon("line", wall.body:getWorldPoints(wall.shape:getPoints()))
+  love.graphics.polygon("line", wall1.body:getWorldPoints(wall1.shape:getPoints()))
+  love.graphics.polygon("line", wall2.body:getWorldPoints(wall2.shape:getPoints()))
+  love.graphics.polygon("line", wall3.body:getWorldPoints(wall3.shape:getPoints()))
   if gotBook == false then
     love.graphics.polygon("line", book.body:getWorldPoints(book.shape:getPoints()))
   end 
