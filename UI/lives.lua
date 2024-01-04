5085a1cfb9cb3048d78de08e2c761035e2680b45
Maffittaa <mafaldam2001@outlook.com>
2024-01-04 16:18:43 +0000

    lives

--- UI/lives.lua
+++ UI/lives.lua
@@ -1,12 +1,37 @@
+
 function DrawHearts()
-    for i = 0, gary.health do
-        local heartSprites = love.graphics.newImage("Sprites/full_heart.png")
-        if gary.health == i - 0.5 then
-            heartSprites = love.graphics.newImage("Sprites/half_heart.png")
-        end
-        if gary.health == i - 1 or gary.health == 0 then
-            heartSprites = love.graphics.newImage("Sprites/empty_heart.png")
-        end
-        love.graphics.draw(heartSprites, camera.x - 700, camera.y - 400)
+    if gary.health == 5 then
+        heartSprites = love.graphics.newImage("Sprites/full_heart.png")
     end
+    if gary.health == 4.5 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_1.png")
+    end
+    if gary.health == 4 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_2.png")
+    end
+    if gary.health == 3.5 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_3.png")
+    end
+    if gary.health == 3 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_4.png")
+    end
+    if gary.health == 2.5 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_5.png") -- half life
+    end
+    if gary.health == 2 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_6.png")
+    end
+    if gary.health == 1.5 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_7.png")
+    end
+    if gary.health == 1 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_8.png")
+    end
+    if gary.health == 0.5 then
+        heartSprites = love.graphics.newImage("Sprites/heart_anim_9.png")
+    end
+    if gary.health == 0 then
+        heartSprites = love.graphics.newImage("Sprites/empty_heart.png")
+    end
+    love.graphics.draw(heartSprites, camera.x - 600, camera.y - 350)
 end
