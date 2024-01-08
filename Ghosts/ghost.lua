418d1d38bbd7f0dc06ff9c9348f7f579063fdce9
Maffittaa <mafaldam2001@outlook.com>
2024-01-08 16:43:57 +0000

    valkyries fixed - Mafalda

--- Ghosts/ghost.lua
+++ Ghosts/ghost.lua
@@ -174,40 +174,6 @@ function UpdateGhost(dt, posicoes)
                 table.remove(posicoes, i + #ghosts)
             end
         end
-
-        --Sound Ghosts
-
-        local magGhostGary = vector.magnitude(vector.sub(gary.position, ghosts[i].position))
-        local distance = 700
-
-        if magGhostGary < distance then
-            local vol
-
-            magGhostGary = distance - magGhostGary
-            vol = magGhostGary / distance
-
-            if not sourceEffect[2]:isPlaying() or not sourceEffect[3]:isPlaying() then
-                if inDarkSide == true then
-
-                elseif inDarkSide == false then
-
-                end
-            end
-
-            if inDarkSide == true then
-                ChangeVol(vol, 2)
-            elseif inDarkSide == false then
-                ChangeVol(vol, 3)
-            end
-        elseif magGhostGary > distance then
-            if sourceEffect[2]:isPlaying() or sourceEffect[3]:isPlaying() then
-                if inDarkSide == true then
-
-                elseif inDarkSide == false then
-
-                end
-            end
-        end
     end
 end
 
--- MainCharacter/healthbar.lua
+++ MainCharacter/healthbar.lua
@@ -16,7 +16,9 @@ end
 
 function UpdateHealthBars()
     for i = 1, #ghosts, 1 do
-        healthbar.ghost[i] = vector2.new(ghosts[i].body:getX() - 35, ghosts[i].body:getY() - 60)
+        if healthbar.ghost[i] < 0 then
+            healthbar.ghost[i] = vector2.new(ghosts[i].body:getX() - 35, ghosts[i].body:getY() - 60)
+        end
     end
 
     for i = #healthbar, 1, -1 do
@@ -63,7 +65,7 @@ function DrawHealthBars()
                 for i = #healthbar, 1, -1 do
                     local num = healthbar[i]
                     if num == 1 and num == 2 then
-                        table.remove(healthbar, i)   
+                        table.remove(healthbar, i)
                     end
                 end
             end
@@ -73,7 +75,8 @@ function DrawHealthBars()
     for i = 1, #valkyries, 1 do
         if valkyries[i].health <= 7 and ghosts[i].health > 0 then
             love.graphics.setColor(1, 0, 0)
-            love.graphics.rectangle("fill", healthbar.valkyrie[i].x, healthbar.valkyrie[i].y, 17.5 * valkyries[i].health, 4)
+            love.graphics.rectangle("fill", healthbar.valkyrie[i].x, healthbar.valkyrie[i].y, 17.5 * valkyries[i].health,
+                4)
         else
             for i = #healthbar, 1, -1 do
                 local num = healthbar[i]
